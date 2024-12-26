16e275c875ec3b8e71b7d2be842a578b5cc6a31d
Chi Tran <khanhchi.tran13051@gmail.com>
2024-12-26 15:16:16 +0100

    LeetCode: 189 Rotate Array 2 solution

--- src/problems/RotateArray189.java
+++ src/problems/RotateArray189.java
@@ -21,3 +21,23 @@ public class RotateArray189 {
         System.out.println(Arrays.toString(nums));
     }
 }
+//public class RotateArray189_2 {
+//    public void rotate(int[] nums, int k){
+//          int n = nums.length;
+//          int r = k % n;
+//          reverse(nums,0, n - 1);
+//          reverse(nums, 0, r - 1);
+//          reverse(nums, r, n - 1);
+//          System.out.println(Arrays.toString(nums));
+//
+//    }
+//    private void reverse(int[] nums, int start, int end){
+//        while(start < end){
+//            int temp = nums[start];
+//            nums[start] = nums[end];
+//            nums[end] = temp;
+//            start++;
+//            end--;
+//        }
+//    }
+//}
