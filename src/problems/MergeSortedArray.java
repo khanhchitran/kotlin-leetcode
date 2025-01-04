48adedaf5763795ba8f2975ace27ee683ecb539c
Chi Tran <khanhchi.tran13051@gmail.com>
2025-01-04 19:57:02 +0100

    LeetCode: 88. Merge Sorted Array

---
+++ src/problems/MergeSortedArray.java
@@ -0,0 +1,26 @@
+package problems;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] merged = new int[m + n];
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        while (i < m) {
+            merged[k++] = nums1[i++];
+        }
+
+        while (j < n) {
+            merged[k++] = nums2[j++];
+        }
+
+        System.arraycopy(merged, 0, nums1, 0, m + n);
+    }
+}
