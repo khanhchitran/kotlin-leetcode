13b21b6c7159e8fc6c546d4a04378a95ae5d91c6
Chi Tran <khanhchi.tran13051@gmail.com>
2025-01-05 19:59:56 +0100

    LeetCode: 26. Remove Duplicates from Sorted Array2

---
+++ src/problems/RemoveDuplicatesFromSortedArray2.java
@@ -0,0 +1,21 @@
+package problems;
+
+public class RemoveDuplicatesFromSortedArray2 {
+    public int removeDuplicates(int[] nums) {
+       int i = 1;
+       int count = 1;
+       for (int j = 1; j < nums.length; j++){
+           if (nums[j] == nums[j-1]){
+               count++;
+           } else {
+               count = 1;
+           }
+
+       if (count <= 2){
+           nums[i] = nums[j];
+           i++;
+       }
+       }
+        return i;
+    }
+}
